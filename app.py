d2396e53e8afd160f2315d2ee063b8af4427d7b1
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 09:35:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Olá, <b>mundo</b>!</p>"
+    return """<h1>Fiscalizando o Ibama</h1>
+    <a href="/sobre">Sobre esse site</a>
+    <p>Visualizando a quantidade de multas ambientais aplicadas no âmbito do Ibama que estão em <b>homologação ou prazo de defesa</b> nos estados que integram o território da Amazônia Legal</p>
+    """
 
 @app.route("/sobre")
 def sobre():
+    return """<h1>Sobre</h1>
+    <a href="/">Página inicial</a>
+    <p>Esse site foi criado por <b>Bianca Muniz</b> como projeto final para obtenção da certificação em Jornalismo de Dados e Automação do Insper.</p>"""
+
+
+@app.route("/teste")
+def teste():
     return "<h1>Sobre</h1> <p>Esse site foi criado por <b>Bianca Muniz</b> como projeto final para obtenção da certificação em Jornalismo de Dados e Automação do Insper.</p>"
