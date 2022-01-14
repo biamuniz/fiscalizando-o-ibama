3669d4050994b1687ef593eb4e49b9722d64ba5f
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 13:11:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,76 @@
-from flask import Flask, request
+import base64
+import json
+import os
+import ssl
+
 import pandas as pd
+import requests
+
+from flask import Flask, render_template
+import gspread
+
 
 app = Flask(__name__)
 
-def dados_multas():
-    df = pd.read_csv("https://dadosabertos.ibama.gov.br/dados/SICAFI/RR/Quantidade/multasDistribuidasBensTutelados.csv", sep=';')
-    df = df.loc[df['Situação Débito'] == 'Para homologação/prazo de defesa']
-    df = df.loc[df['Tipo Auto'] == 'Multa']
-    del df['Nº AI']
-    del df['Data Auto']
-    del df['Enquadramento Legal']
-    dados = df.groupby(['Município', 'Tipo Infração', 'Última Atualização Relatório']).size().to_frame(name = 'count').reset_index().sort_values(by=['count'],ascending=False)
-    return dados
- 
+spreadsheet_id = os.environ["SPREADSHEET_ID"]
+conteudo_codificado = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+service_account = gspread.service_account_from_dict(credentials)
+
+amazonia = ['AC', 'AM', 'AP','MA','PA','MT','RR','RO','TO']
+def get_multas_amazonia_legal():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    UF = input("Digite a sigla da unidade de federação: ")
+    UF = UF.upper()
+    if UF in amazonia:
+      url = "https://dadosabertos.ibama.gov.br/dados/SICAFI/"+UF+"/Quantidade/multasDistribuidasBensTutelados.csv"
+      df = pd.read_csv(url, sep=';')
+      spreadsheet = service_account.open_by_key(spreadsheet_id)
+      worksheet = spreadsheet.worksheet(UF)
+      df = df.loc[df['Situação Débito'] == 'Para homologação/prazo de defesa']
+      df = df.loc[df['Tipo Auto'] == 'Multa']
+      del df['Nº AI']
+      del df['Data Auto']
+      del df['Enquadramento Legal']
+      dados = df.groupby(['Município', 'Tipo Infração', 'Última Atualização Relatório']).size().to_frame(name = 'count').reset_index().sort_values(by=['count'],ascending=False)
+      worksheet.update([dados.columns.values.tolist()] + dados.values.tolist())
+    else:
+      print("Esta UF não pertence ao território da Amazônia Legal!")
+    return UF
 
 @app.route("/")
 def hello_world():
-    return """
-        <h1>Fiscalizando o Ibama</h1>
-        <a href="/teste">Testes</a> - <a href="/sobre">Sobre esse site</a>
-        <p>
-            Visualizando a quantidade de multas ambientais aplicadas no âmbito do Ibama que estão em <b>homologação ou prazo de defesa</b> nos estados que integram o território da Amazônia Legal.
-        </p>
-    """
-
-@app.route('/multas', methods = ['POST', 'GET']) 
-def multas(): 
-   if request.method == 'POST': 
-    UF = request.form['UF']  
-    return
-    """<body> 
-    <form action = "UF" method = "POST">
-        <p>Estado <input type = "text" name = "UF" /></p> 
-         <p><input type = "submit" value = "submit" /></p> 
-      </form>        
-   </body>"""
+    arquivo = open("templates/home.html")
+    return arquivo.read()
 
 @app.route("/sobre")
 def sobre():
-    return """
-        <h1>Sobre</h1>
-        <a href="/teste">Testes</a> - <a href="/">Página inicial</a>
-        <p>
-            Esse site foi criado por <b>Bianca Muniz</b> como projeto final para obtenção da certificação em Jornalismo de Dados e Automação do Insper.
-        </p>
-            """
+    arquivo = open("templates/sobre.html")
+    return arquivo.read()
 
-@app.route("/teste")
-def teste():
-    return """
-        <h1>Teste</h1>
-        <a href="/sobre">Sobre esse site</a> - <a href="/">Página inicial</a>
-        <p>
-            Teste de visualização
-        </p>
-        <div class="flourish-embed flourish-chart" data-src="visualisation/8391891"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
-            """
-
-@app.route("/dados")
-def dados_multas():
-    dados = dados_multas()
-    return f"""
-    <h1>Multas no Pará</h1> 
-    <a href="/sobre">Sobre esse site</a>
-    <p> Dados
-        {dados}
-    </p>"""
+@app.route("/multas")
+def multas():
+    UF = get_multas_amazonia_legal()
+    if UF = "AC":
+        arquivo = open("templates/multasac.html")
+    elif UF = "AM":
+        arquivo = open("templates/multasam.html")
+    elif UF = "AP":
+        arquivo = open("templates/multasap.html")
+    elif UF = "MA":
+        arquivo = open("templates/multasma.html")
+    elif UF = "MT":
+        arquivo = open("templates/multasmt.html")
+    elif UF = "PA":
+        arquivo = open("templates/multaspa.html")
+    elif UF = "RO":
+        arquivo = open("templates/multasro.html")
+    elif UF = "RR":
+        arquivo = open("templates/multasrr.html")
+    elif UF = "TO":
+        arquivo = open("templates/multasto.html")
+    else:
+        arquivo = open("templates/erro.html")
+    return arquivo.read()
 
