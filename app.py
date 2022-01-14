d20c11913d0680ed6503862a314b354d1ae06d6e
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 10:39:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return """
         <h1>Fiscalizando o Ibama</h1>
-        <a href="/teste">Testes</a> -- <a href="/sobre">Sobre esse site</a>
+        <a href="/teste">Testes</a> - <a href="/sobre">Sobre esse site</a>
         <p>
             Visualizando a quantidade de multas ambientais aplicadas no âmbito do Ibama que estão em <b>homologação ou prazo de defesa</b> nos estados que integram o território da Amazônia Legal.
         </p>
@@ -17,19 +17,8 @@ def hello_world():
 def sobre():
     return """
         <h1>Sobre</h1>
-        <a href="/teste">Testes</a> -- <a href="/">Página inicial</a>
+        <a href="/teste">Testes</a> - <a href="/">Página inicial</a>
         <p>
             Esse site foi criado por <b>Bianca Muniz</b> como projeto final para obtenção da certificação em Jornalismo de Dados e Automação do Insper.
-        </p>
-            """
-
-    
-@app.route("/teste")
-def get_multas_amazonia_legal():  
-    return """
-        <h1>Multas na Amazônia Legal</h1>
-        <a href="/sobre">Sobre esse site</a> -- <a href="/">Página inicial</a>
-        <p>
-            <div class="flourish-embed flourish-chart" data-src="visualisation/8391891"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
         </p>
             """
