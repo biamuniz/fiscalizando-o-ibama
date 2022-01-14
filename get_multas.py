7f0811a5d84da2fcac32951a96b46b09e67f1c3a
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 15:03:59 -0300

    Create get_multas.py

---
+++ get_multas.py
@@ -0,0 +1,34 @@
+import base64
+import json
+import os
+import pandas as pd
+import ssl
+import gspread
+import requests
+
+spreadsheet_id = os.environ["SPREADSHEET_ID"]
+conteudo_codificado = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+service_account = gspread.service_account_from_dict(credentials)
+amazonia = ['AC', 'AM', 'AP','MA','PA','MT','RR','RO','TO']
+
+def get_multas():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    UF = input("Digite a sigla da unidade de federação: ")
+    UF = UF.upper()
+    if UF in amazonia:
+      url = "https://dadosabertos.ibama.gov.br/dados/SICAFI/"+UF+"/Quantidade/multasDistribuidasBensTutelados.csv"
+      df = pd.read_csv(url, sep=';')
+      spreadsheet = service_account.open_by_key(spreadsheet_id)
+      worksheet = spreadsheet.worksheet(UF)
+      df = df.loc[df['Situação Débito'] == 'Para homologação/prazo de defesa']
+      df = df.loc[df['Tipo Auto'] == 'Multa']
+      del df['Nº AI']
+      del df['Data Auto']
+      del df['Enquadramento Legal']
+      dados = df.groupby(['Município', 'Tipo Infração', 'Última Atualização Relatório']).size().to_frame(name = 'count').reset_index().sort_values(by=['count'],ascending=False)
+      worksheet.update([dados.columns.values.tolist()] + dados.values.tolist())
+    else:
+      print("Esta UF não pertence ao território da Amazônia Legal!")
+    return UF
