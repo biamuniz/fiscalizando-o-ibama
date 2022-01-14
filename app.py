ec6ce05f2b8bd8469bdaec2b6146c20bc12306eb
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 10:09:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,25 @@ def sobre():
             Esse site foi criado por <b>Bianca Muniz</b> como projeto final para obtenção da certificação em Jornalismo de Dados e Automação do Insper.
         </p>
             """
+
+@app.route("/amazonia")
+def get_multas_amazonia_legal():  
+    UF = input("Digite a sigla da unidade de federação: ")
+    UF = UF.upper()
+    if UF in amazonia:
+      url = "https://dadosabertos.ibama.gov.br/dados/SICAFI/"+UF+"/Quantidade/multasDistribuidasBensTutelados.csv"
+      df = pd.read_csv(url, sep=';')
+      spreadsheet = service_account.open_by_key(spreadsheet_id) # "abrir" o arquivo
+      worksheet = spreadsheet.worksheet(UF) # escolhe uma aba
+      df = df.loc[df['Situação Débito'] == 'Para homologação/prazo de defesa']
+      df = df.loc[df['Tipo Auto'] == 'Multa']
+      # deletando colunas que não são necessárias na visualização
+      del df['Nº AI']
+      del df['Data Auto']
+      del df['Enquadramento Legal']
+      dados = df.groupby(['Município', 'Tipo Infração', 'Última Atualização Relatório']).size().to_frame(name = 'count').reset_index().sort_values(by=['count'],ascending=False)
+      worksheet.update([dados.columns.values.tolist()] + dados.values.tolist())
+    else:
+      print("Esta UF não pertence ao território da Amazônia Legal!")
+    return UF
+            """
