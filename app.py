c2b2461c4a68130c55af8c3f7b8de7d69590b866
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 09:17:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,3 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Olá, <b>mundo</b>!</p>"
 
+@app.route("/sobre")
+def sobre():
+    return "<h1>Sobre</h1> <p>Esse site foi criado por <b>Bianca Muniz</b> como projeto final para obtenção da certificação em Jornalismo de Dados e Automação do Insper.</p>"
