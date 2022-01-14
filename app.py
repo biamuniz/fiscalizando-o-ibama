2248fccaeacda322d1aa98ca99428bd7fe41a805
Bianca <41485608+biamuniz@users.noreply.github.com>
2022-01-14 17:39:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-import base64
-import json
-import os
-import pandas as pd
-import ssl
-
-from get_multas import get_multas_amazonia
-
 import gspread
-import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-spreadsheet_id = os.environ["SPREADSHEET_ID"]
-conteudo_codificado = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
-conteudo = base64.b64decode(conteudo_codificado)
-credentials = json.loads(conteudo)
-service_account = gspread.service_account_from_dict(credentials)
-amazonia = ['AC', 'AM', 'AP','MA','PA','MT','RR','RO','TO']
+
 
 @app.route("/")
 def hello_world():
